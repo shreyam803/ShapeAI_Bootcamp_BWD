9fd93446af2bc74af115ccc6bcbdc3a0f03fc565
Shreya Maheshwari <shreyam803@gmail.com>
2021-06-26 16:40:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+       <div className="note">
+      <h1>Javascript with Node.js</h1>
+      <p>A basic web dev Node.js Bootcamp.</p>
+      </div>
+  );
+}
+
+export default Info;
